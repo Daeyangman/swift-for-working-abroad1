74658163187d91c8b28a0a61b0d4394e402884a4
Daeyangman <ha862531@gmail.com>
2017-03-13 23:08:17 +0900

    minihomework1

--- 01_basic_operators.playground/Contents.swift
+++ 01_basic_operators.playground/Contents.swift
@@ -2,23 +2,20 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var country = [String]()
+country.append("미국")
+country.append("캐나다")
+//국가 리스트로 list , set 형태 무관
 
-var number1 = 1
-number1
+var koreanFood = [String]()
+koreanFood.append("양념치킨")
+koreanFood.append("간장게장")
+koreanFood.append("부대찌개")
+// 좋아하는 음식의 나열로 list ,set 형태 무관 
 
-let number2 = 2
-
-
-
-1+2
-5-3
-2*3
-10.0/2.5
-
-"abc" + "def"
-
-var a = 3
-
-a += 2
-
+var charFruit = [
+    "바나나" : "흡수가 빠르다",
+    "망고" : "달고 칼로리가높다" ,
+    "수박" : "수분이 많다"
+]
+// 과일별(키) 특징(밸류)이 필요하므로 딕셔너리 타입이 적절 
