a468d0be7b5d1d53dd325dc565cbae798f563d8b
Daeyangman <ha862531@gmail.com>
2017-03-14 23:37:59 +0900

    homework1

---
+++ 09_Homework_1.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var a : Int = 46
+
+if a < 0 {
+    print("점수가 아닙니다")
+} else if a < 25 {
+    print("점수는 F입니다")
+} else if a < 50 {
+    print("점수는 C입니다")
+} else if a < 75 {
+    print("점수는 B입니다")
+} else if a <= 100 {
+    print("점수는 A입니다")
+} else  {
+    print("점수가 아닙니다")
+}
