a9a26b877f82612272c16977d138ba036644d6b3
Daeyangman <ha862531@gmail.com>
2017-03-14 22:23:41 +0900

    minihomework2

---
+++ 07_homework_2.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func favoriteFood(_ food:String)-> String {
+    return("나는 \(food)를 좋아합니다")
+}
+
+print(favoriteFood("치킨"))
+
+
